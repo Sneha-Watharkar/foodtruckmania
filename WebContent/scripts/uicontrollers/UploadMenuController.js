1c211d1f5edc57a4b8c2379baba154f9c2f2e517
swathark <swathark@uncc.edu>
2017-12-03 19:02:31 -0500

    Upload menu params changed

--- WebContent/scripts/uicontrollers/UploadMenuController.js
+++ WebContent/scripts/uicontrollers/UploadMenuController.js
@@ -7,28 +7,28 @@ mainApp.controller('uploadMenuController', function($scope, $location, $window, 
 			return ($location.path().substr(0, path.length) == path) ? 'active' : '';
 		}
         
+        $scope.currentUser = UserService.getCurrentUser();
+        console.log("In UploadMenuCtrl", $scope.currentUser);
         
         $scope.uploadMenu = function () {
         	var fd = new FormData();
 	        fd.append('file', $scope.menuFile);
+	        fd.append('userId', $scope.currentUser.userId);
+	        fd.append('action','uploadMenu');
 	        console.log("File here is ",$scope.menuFile);
 	        $scope.displayNotification('show', 'Menu Uploaded successfully!', 'success');
 	        /*var file = new Blob([$scope.menuFile], {type: 'application/pdf'});
 	  	       var fileURL = URL.createObjectURL(file);
 	        $scope.foodMenuFile = $sce.trustAsResourceUrl(fileURL);
 	        $window.open($scope.foodMenuFile);*/
-	        /*$http.post('uploadMenu', fd, {
+	        $http.post('uploadMenu', fd, {
 	             transformRequest: angular.identity,
 	             headers: {'Content-Type': undefined}
-	        });*/
+	        });
         }
         
         $scope.displayNotification = function(status, message, type) {
-        	$scope.notification = {
-					/*status: 'hide',
-					type: '',
-					message: ''*/
-			};
+        	$scope.notification = {};
 	        $scope.notification.status = status; 
 	        $scope.notification.message = message;
 	        $scope.notification.type = type;
