6ad0d80cd1d77c724023cbd4467aa5b3857b0055
OmSriSaiRam\Durga <dabayakumar>
2017-12-05 10:39:19 -0500

    added db call for pulling menu info.

--- src/db/MenuUpdate.java
+++ src/db/MenuUpdate.java
@@ -5,8 +5,10 @@ import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import dbgateway.TableDataGateway;
+import util.ConnectionPool2;
 import util.DBUtil;
 
 public class MenuUpdate {
@@ -26,6 +28,30 @@ public class MenuUpdate {
 		} catch (Exception e) {
 			System.out.println(e);
 			return 0;
+		} finally {
+			DBUtil.closePreparedStatement(ps);
+		}
+	}
+	
+	public static String displayMenu(int userId) {
+		PreparedStatement ps = null;
+		String query = "SELECT foodTruckMenu FROM FoodTruck " +  "where userId = ?";
+		String filePath;
+		try {
+			Connection connection = ConnectionPool2.getConnection();
+			System.out.println(connection.toString());
+			ps = connection.prepareStatement(query);
+			System.out.println(ps.toString());
+			ps.setInt(1, userId);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				filePath = rs.getString("foodTruckMenu");
+				return filePath;
+			}
+			return null;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
 		} finally {
 			DBUtil.closePreparedStatement(ps);
 		}
